43624b3272fb5c36dff56087a851927cddfa08aa
memeperez <31419406+memeperez@users.noreply.github.com>
2017-08-28 14:45:06 -0500

    Add files via upload

---
+++ 01/sketch.js
@@ -0,0 +1,615 @@
+function setup() { 
+  createCanvas(389, 711);
+  background(244,232,211);
+    
+  
+  //BASICAS//
+  
+  //Fondos lineas//
+      
+  // Numero 1//
+  fill(238,233,233);
+  strokeWeight(1);
+  beginShape();
+  vertex(0, 245.33);
+  vertex(389, 40.67);
+  vertex(389,76.25);
+  vertex(0,284);
+  vertex(0, 245.33);
+  endShape();
+  
+    // Numero 2//
+  fill(238,233,233);
+  strokeWeight(1);
+  beginShape();
+  vertex(0, 284);
+  vertex(389, 76.25);
+  vertex(389,115.5);
+  vertex(0,318);
+  vertex(0, 282);
+  endShape();
+  
+     // Numero 3//
+  fill(238,233,233);
+  strokeWeight(1);
+  beginShape();
+  vertex(0,318);
+  vertex(132.55, 249);
+  vertex(145.67,278);
+  vertex(0,355.5);
+  vertex(0, 318);
+  endShape();
+  
+       // Numero 4//
+  fill(238,233,233);
+  strokeWeight(1);
+  beginShape();
+  vertex(0,355);
+  vertex(132.88, 284.81);
+  vertex(147.67,312.33);
+  vertex(0,390.33);
+  vertex(0,318);
+  endShape();
+  
+    //C R//
+  strokeWeight(1);
+  stroke(4,4,5);
+  line(93.82,195.97,111.64, 221.95);
+  
+  strokeWeight(1);
+  stroke(4,4,5);
+  line(128.8,177.57,147.22, 203.26);
+  
+  //relleno de puntos//
+  
+  
+    //LO 2
+  fill(198,128,60);
+  strokeWeight(1);
+  beginShape();
+  vertex(0,485.6);
+  vertex(0,443.5);
+  vertex(146,711);
+  vertex(135, 711); 
+  vertex(0,486.5);  
+  vertex(0,485.6);  
+  endShape();
+  
+    
+       // Numero 5//
+  fill(238,233,233);
+  strokeWeight(1);
+  beginShape();
+  vertex(0,425,5);
+  vertex(140.89, 334.12);
+  vertex(132.88,370.8);
+  vertex(0, 458.5);
+  vertex(0,425,5);
+  endShape();
+  
+  //Fondos lineas oscuras//
+  //LO 1 
+  fill(198,128,60);
+  strokeWeight(1);
+  beginShape();
+  vertex(0,390.33);
+  vertex(321,220.5);
+  vertex(0, 423);
+  vertex(0, 390.33);
+  endShape();
+  
+
+      //LO 3 // 
+    //LO 2 
+  fill(198,128,60);
+  strokeWeight(1);
+  beginShape();
+  vertex(0,561.67);
+  vertex(88.33,711);
+  vertex(73.83, 711);
+  vertex(0, 596);
+  endShape(CLOSE);
+  
+       //LO 4
+  fill(198,128,60);
+  strokeWeight(1);
+  beginShape();
+  vertex(0,650);
+  vertex(0,618.67);
+  vertex(54,711);
+  vertex(38.33,711); 
+  vertex(0,650);  
+  
+         //LO 5
+  fill(198,128,60);
+  strokeWeight(1);
+  beginShape();
+  vertex(0,650);
+  vertex(0,618.67);
+  vertex(54,711);
+  vertex(38.33,711); 
+  vertex(0,650);  
+  endShape();
+  
+// primer rectangulo compuesto//
+  fill(247,243,200);
+  noStroke(); 
+  rect(33.25,69.5,56.25,56.25);
+  
+  fill(220,229,236);
+  noStroke(); 
+  rect(39.5,76.25,44.25,39.25);
+  
+  fill(18,20,14);
+  noStroke(); 
+  rect(45.75,81.5,32.229,30.5);
+  
+  fill(235,206,155);
+  noStroke(); 
+  rect(51.25,87,21.75,19.92);
+  
+  //Circulo color tomate de arbol//
+  fill(201,101,46);
+  strokeWeight(0.25);
+  stroke(4,4,5);
+  ellipse(-33.022,286.227,104.88,104.88)
+  
+  //fondo oscururo para el cuadrado mini//
+           // Numero 4//
+  fill(4,2,2);
+  strokeWeight(1);
+  beginShape();
+  vertex(132.88,284.81);
+  vertex(176.25,261.75);
+  vertex(193.25, 287.75);
+  vertex(147.67, 312.33);
+  vertex(132.88,284.81);
+  endShape();
+  
+   //Circulo AmaAzul//
+  fill(237,206,114);
+  strokeWeight(0.25);
+  stroke(4,4,5);
+  ellipse(174.209,242.862,93.73, 93.73)
+  
+  fill(110,163,220);
+  strokeWeight(0.25);
+  stroke(4,4,5);
+  ellipse(174.209,242.862,75.609, 75.609); 
+  
+  //Cuadrado oscuro mini//
+  
+         // Numero 4//
+  fill(143,58,26, 70);
+  strokeWeight(1);
+  beginShape();
+  vertex(132.88,284.81);
+  vertex(176.25,261.75);
+  vertex(193.25, 287.75);
+  vertex(147.67, 312.33);
+  vertex(132.88,284.81);
+  endShape();
+  
+  //color oscuro del cuadrado de abajo//
+  
+    //Cuadrado relleno con puntos//
+  fill(4,2,2);
+  strokeWeight(1);
+  beginShape();
+  vertex(102.93,390.3);
+  vertex(149.75,360.75);
+  vertex(176.25,407.5);
+  vertex(131.5, 437); 
+  vertex(102.93,390.3);
+  endShape();
+  
+    //Circulos ROJO Y ROSADO/
+  fill(183,26,23);
+  strokeWeight(0.25);
+  stroke(4,4,5);
+  ellipse(177.418,362.697,89.273, 89.273)
+  
+  fill(189,136,157);
+  strokeWeight(0.25);
+  stroke(4,4,5);
+  ellipse(175.418,359.697,81.273, 81.273)
+  
+  //Cuadrado relleno con puntos//
+  fill(4,2,2,170);
+  strokeWeight(1);
+  beginShape();
+  vertex(102.93,390.3);
+  vertex(149.75,360.75);
+  vertex(176.25,407.5);
+  vertex(131.5, 437); 
+  vertex(102.93,390.3);
+  endShape();
+  
+  //puntos claros //
+  //1
+  fill(240,234,236);
+  strokeWeight(1);
+  beginShape();
+  vertex(111.17,386.92);
+  vertex(113.58,386.33);
+  vertex(114.25,389.75);
+  vertex(112.71, 391.67); 
+  vertex(110.92,390.33);
+  vertex(111.17,386.92);
+  endShape();
+  
+  //2
+  fill(240,234,236);
+  strokeWeight(1);
+  beginShape();
+  vertex(118.38,387.25);
+  vertex(120.08,386.69);
+  vertex(120.54,388.58);
+  vertex(119, 389); 
+  vertex(119,390.33);
+  vertex(118.69,392.19);
+  vertex(117.44,390.33);
+  vertex(117.69,388.52);
+  vertex(118.38,387.25);
+  endShape();
+  
+    //3
+  fill(240,234,236);
+  strokeWeight(1);
+  beginShape();
+  vertex(126.17,383.92);
+  vertex(128.25,383.92);
+  vertex(129.25,385.25);
+  vertex(129.25,389.92);
+  vertex(127.25,392.08);
+  vertex(124.08,393.25);
+  vertex(125.17,389.92);
+  vertex(126.25,388.58);
+  vertex(126.17,383.92);
+  endShape();
+  
+  //4
+  fill(240,234,236);
+  strokeWeight(1);
+  beginShape();
+  vertex(120.08,395.08);
+  vertex(122.75,395.08);
+  vertex(123.33,397.92);
+  vertex(124.08,398.88);
+  vertex(124.08,403);
+  vertex(122.75,403);
+  vertex(121.5,401.75);
+  vertex(120.08,400.67);
+  vertex(120.5,397.83);
+  vertex(120.08,395.08);
+  endShape();
+  
+  //5
+  fill(240,234,236);
+  strokeWeight(1);
+  beginShape();
+  vertex(135,397.83);
+  vertex(137.17,398.88);
+  vertex(137.17,401.92);
+  vertex(139.17,403.42);
+  vertex(140.33,404.92);
+  vertex(138.92, 406.92);
+  vertex(138.08,408.92);
+  vertex(135,408.92);
+  vertex(134.42,406);
+  vertex(133.58,403.75);
+  vertex(136.67,402.17);
+  vertex(135,397.83);
+  endShape();
+  
+  //6
+  fill(240,234,236);
+  strokeWeight(1);
+  beginShape();
+  vertex(140.89,407.75);
+  vertex(143.42,407.75);
+  vertex(143.67,409.42);
+  vertex(143.25,411.67);
+  vertex(144.25,410.67);
+  vertex(145.42, 411.25);
+  vertex(144.92,412.71);
+  vertex(143.75,413.54);
+  vertex(143.75,415.25);
+  vertex(141.92,415.25);
+  vertex(141.75,414.17);
+  vertex(140.89,412.71);
+  vertex(140.89,411.92);
+  vertex(140.33,410.83);
+  vertex(140.89,408.92);
+  vertex(140.89,407.75);
+  endShape(CLOSE);
+  
+  
+     //Circulos BLANCO Y NEGRO/
+  fill(4,2,1);
+  strokeWeight(0.25);
+  stroke(4,4,5);
+  ellipse(314.156,281.78,74.56, 74.56);
+  
+  fill(252,245,249);
+  strokeWeight(0.25);
+  stroke(4,4,5);
+  ellipse(313.889,281.382,66.236, 66.236);
+  
+//LINEAS ASCENDENTES//
+//Linea 1//
+  strokeWeight(4);
+  stroke(4,4,5);
+  line(0,244.67,389,39.33);
+  
+  //cuadrado Gris//
+  fill(116,111,123);
+  strokeWeight(1);
+  beginShape();
+  vertex(259.25,108.25);
+  vertex(296.5,88);
+  vertex(313.75,119);
+  vertex(275.5,138);
+  vertex(259.25,108.25);
+  endShape();
+  
+  
+//Linea 2//
+  strokeWeight(4);
+  stroke(4,4,5);
+  line(0,286,389,77.25);
+  
+  //Linea baja//
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(0,277.5,54.14,326.74);
+  
+   //Linea mini//
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(0,347.17,3.17,352.33);
+  
+   //Linea pegadita//
+  strokeWeight(3);
+  stroke(4,4,5);
+  line(0,276.5,0,302.83);
+  
+//Linea 3//
+  strokeWeight(4);
+  stroke(4,4,5);
+  line(0,316.67,389,115.5);
+  
+//Linea 4//
+  strokeWeight(4);
+  stroke(4,4,5);
+  line(0,355.5,176,262);
+  
+//Linea 5//
+  strokeWeight(4);
+  stroke(4,4,5);
+  line(176,262,191.67,288.67);
+  
+//Linea 5//
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(0,391.33,331.33,216.08);
+  
+  
+//Linea 6//
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(0,422,331.33,216.08);
+  
+  
+//Linea 7//
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(0,457.67,148.33,361.67);
+  
+  
+  
+//lINEAS DESCENDENTES//
+//Linea 1// 
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(0,442.67,148.33,711);
+  
+//Linea 2// 
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(0,486,137,711);
+  
+//Linea 3// 
+  strokeWeight(4);
+  stroke(4,4,5);
+  line(0,559,88.5,711);  
+  
+//Linea 4// 
+  strokeWeight(3);
+  stroke(4,4,5);
+  line(0,596,73.67,711);   
+  
+//Linea 5// 
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(0,618.5,54,711);    
+  
+//Linea 6// 
+  strokeWeight(11);
+  stroke(4,4,5);
+  line(0,692.33,68.5,711);      
+  
+//lINEAS A compuesta//  
+  strokeWeight(3);
+  stroke(4,4,5);
+  line(0,611.5,6.2,607.09);  
+  
+  strokeWeight(3);
+  stroke(4,4,5);
+  line(6.2,607.09,21.19,595.39);  
+  
+  strokeWeight(3);
+  stroke(4,4,5);
+  line(21.19,595.39,52.5,572.23); 
+  
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(52.5, 572.23, 65.42, 562.01); 
+  
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(0, 624.63, 26.5, 604.51); 
+  
+  strokeWeight(0.5);
+  stroke(4,4,5);
+  line(26.5, 604.51, 29.54, 603.08); 
+  
+  strokeWeight(0.5);
+  stroke(4,4,5);
+  line(29.36, 603.17, 29.9, 601.9); 
+  
+  strokeWeight(0.5);
+  stroke(4,4,5);
+  line(29.9, 602.13, 52.5, 584.63); 
+  
+  strokeWeight(0.5);
+  stroke(4,4,5);
+  line(52.5, 584.63, 58.66, 582.34); 
+  
+  strokeWeight(0.5);
+  stroke(4,4,5);
+  line(58.66, 582.34, 71.73, 573.36); 
+  
+  fill(4,4,5);
+  strokeWeight(1);
+  beginShape();
+  vertex(66.82,561.17);
+  vertex(73.67,573.36);
+  vertex(296,407);
+  vertex(285.75,394.25);
+  endShape(CLOSE); 
+  
+  //TRIKI INFERIOR//
+  
+  //cuadrados de color//
+  //c1
+  fill(237,233,237);
+  noStroke(); 
+  rect(258,529.67,47.33,47.33);
+  
+  //c2
+  fill(246,77,91);
+  noStroke(); 
+  rect(306.33,530.68,47.33,47.33);
+  
+  //C3
+  fill(254,171,158);
+  strokeWeight(1);
+  beginShape();
+  vertex(257,576);
+  vertex(305,576);
+  vertex(305, 625.33);
+  vertex(255,625.33);
+  vertex(255,577);
+  endShape();
+ 
+    //C4
+  fill(222,221,229);
+  strokeWeight(1);
+  beginShape();
+  vertex(305.63, 579);
+  vertex(353.67,580.67);
+  vertex(353.67, 629.33);
+  vertex(304.67, 627.67);
+  vertex(305.63, 579);
+  endShape();
+  
+  //GRUPO1
+  
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(260, 463.5, 252.5, 701.5); 
+  
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(307.5, 463.5, 303.5, 701.5); 
+  
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(354.5, 463.5, 354, 701.5); 
+  
+  //GRUPO2
+  
+
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(178.5, 526.5, 389, 532.5); 
+  
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(174.5, 572.23, 389, 580.5); 
+  
+  strokeWeight(2);
+  stroke(4,4,5);
+  line(174.5, 621.33, 389, 630); 
+  
+  //CIRCULO AZUL//
+  fill(3,97,163,250);  
+  noStroke();
+  ellipse(2.782,587.239,131.436, 131.436);
+   
+  
+  //Cuadrados//
+  
+  //C 1//
+
+  fill(4,4,5);
+  strokeWeight(1);
+  beginShape();
+  vertex(225,200.33);
+  vertex(255,185);
+  vertex(272.33,213.33);
+  vertex(241,230.67);
+  vertex(225,200.33);
+  endShape();
+  
+  //C 2//
+  fill(4,4,5);
+  strokeWeight(1);
+  beginShape();
+  vertex(222.17,283.55);
+  vertex(246.6,268.6);
+  vertex(260.5,291.5);
+  vertex(236,305.17);
+  vertex(221,281.5);
+  endShape();
+  
+  // C 3//
+  fill(4,4,5);
+  strokeWeight(1);
+  beginShape();
+  vertex(239.3,320.5);
+  vertex(267.17,305.17);
+  vertex(283.67,334);
+  vertex(256.67,350.83);
+  vertex(239.3,320.5);
+  endShape();
+  
+    // C 4//
+  fill(4,4,5);
+  strokeWeight(1);
+  beginShape();
+  vertex(141.75,451.25);
+  vertex(185,423);
+  vertex(204.5,454.25);
+  vertex(161.25,484.75);
+  vertex(141.75,451.25);
+  endShape();
+  
+  
+  //LINEA DE DETALLE <3//
+  
+  strokeWeight(3);
+  stroke(4,4,5);
+  line(198.33,455.17,208.83,458); 
+} 
