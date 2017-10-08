ed9a2759a0c4fd22693f1b7f5739fd28aca0f825
memeperez <31419406+memeperez@users.noreply.github.com>
2017-10-08 09:25:55 -0500

    Add files via upload

---
+++ 05/sketch.js
@@ -0,0 +1,325 @@
+var w = 20;
+
+
+var x = 140;
+var x2 = 500;
+var x3 = 800;
+var y = 200;
+
+var misSegundos = second();
+var misMinutos = minute();
+var misHoras = hour();
+
+
+
+function setup() {
+  createCanvas(900, 400);
+}
+
+function draw() {
+  background(0);
+
+
+  var misSegundos = second();
+  var misMinutos = minute();
+  var misHoras = hour();
+
+
+
+
+  //Dibujo para contar horas//
+
+
+  if (misHoras >= 1 && misHoras <= 1) {
+
+    for (var circulo = 1; circulo > 0; circulo = circulo - 1) {
+      stroke(255, 255, 255);
+      ellipse(x, y, w, w);
+    }
+  }
+
+
+  if (misHoras >= 2 && misHoras <= 2) {
+
+    for (var circulo2 = 2; circulo2 > 0; circulo2 = circulo2 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo2, 20 * circulo2);
+    }
+  }
+
+
+  if (misHoras >= 3 && misHoras <= 3) {
+
+    for (var circulo3 = 3; circulo3 > 0; circulo3 = circulo3 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo3, 20 * circulo3);
+    }
+  }
+
+  if (misHoras >= 4 && misHoras <= 4) {
+
+    for (var circulo4 = 4; circulo4 > 0; circulo4 = circulo4 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo4, 20 * circulo4);
+    }
+  }
+
+  if (misHoras >= 5 && misHoras <= 5) {
+
+    for (var circulo5 = 5; circulo5 > 0; circulo5 = circulo5 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo5, 20 * circulo5);
+    }
+  }
+
+  if (misHoras >= 6 && misHoras <= 6) {
+
+    for (var circulo6 = 6; circulo6 > 0; circulo6 = circulo6 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo6, 20 * circulo6);
+    }
+  }
+
+  if (misHoras >= 7 && misHoras <= 7) {
+
+    for (var circulo7 = 7; circulo7 > 0; circulo7 = circulo7 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo7, 20 * circulo7);
+    }
+  }
+
+  if (misHoras >= 8 && misHoras <= 8) {
+
+    for (var circulo8 = 8; circulo8 > 0; circulo8 = circulo8 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo8, 20 * circulo8);
+    }
+  }
+
+  if (misHoras >= 9 && misHoras <= 9) {
+
+    for (var circulo9 = 9; circulo9 > 0; circulo9 = circulo9 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo9, 20 * circulo9);
+    }
+  }
+
+  if (misHoras >= 10 && misHoras <= 10) {
+
+    for (var circulo10 = 10; circulo10 > 0; circulo10 = circulo10 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo10, 20 * circulo10);
+    }
+  }
+
+  if (misHoras >= 11 && misHoras <= 11) {
+
+    for (var circulo11 = 11; circulo11 > 0; circulo11 = circulo11 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo11, 20 * circulo11);
+    }
+  }
+
+  if (misHoras >= 12 && misHoras <= 12) {
+
+    for (var circulo12 = 12; circulo12 > 0; circulo12 = circulo12 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo12, 20 * circulo12);
+    }
+  }
+
+  if (misHoras >= 13 && misHoras <= 13) {
+
+    for (var circulo13 = 1; circulo13 > 0; circulo13 = circulo13 - 1) {
+      stroke(255, 255, 255);
+      ellipse(x, y, w, w);
+    }
+  }
+
+
+  if (misHoras >= 14 && misHoras <= 14) {
+
+    for (var circulo14 = 2; circulo14 > 0; circulo14 = circulo14 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo14, 20 * circulo14);
+    }
+  }
+
+
+  if (misHoras >= 15 && misHoras <= 15) {
+
+    for (var circulo15 = 3; circulo15 > 0; circulo15 = circulo15 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo15, 20 * circulo15);
+    }
+  }
+
+  if (misHoras >= 16 && misHoras <= 16) {
+
+    for (var circulo16 = 4; circulo16 > 0; circulo16 = circulo16 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo16, 20 * circulo16);
+    }
+  }
+
+  if (misHoras >= 17 && misHoras <= 17) {
+
+    for (var circulo17 = 5; circulo17 > 0; circulo17 = circulo17 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo17, 20 * circulo17);
+    }
+  }
+
+  if (misHoras >= 18 && misHoras <= 18) {
+
+    for (var circulo18 = 6; circulo18 > 0; circulo18 = circulo18 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo18, 20 * circulo18);
+    }
+  }
+
+  if (misHoras >= 19 && misHoras <= 19) {
+
+    for (var circulo19 = 7; circulo19 > 0; circulo19 = circulo19 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo19, 20 * circulo19);
+    }
+  }
+
+  if (misHoras >= 20 && misHoras <= 20) {
+
+    for (var circulo20 = 8; circulo20 > 0; circulo20 = circulo20 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo20, 20 * circulo20);
+    }
+  }
+
+  if (misHoras >= 21 && misHoras <= 21) {
+
+    for (var circulo21 = 9; circulo21 > 0; circulo21 = circulo21 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo21, 20 * circulo21);
+    }
+  }
+
+  if (misHoras >= 22 && misHoras <= 22) {
+
+    for (var circulo22 = 10; circulo22 > 0; circulo22 = circulo22 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo22, 20 * circulo22);
+    }
+  }
+
+  if (misHoras >= 23 && misHoras <= 23) {
+
+    for (var circulo23 = 11; circulo23 > 0; circulo23 = circulo23 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo23, 20 * circulo23);
+    }
+  }
+
+  if (misHoras >= 24 && misHoras <= 24) {
+
+    for (var circulo24 = 12; circulo24 > 0; circulo24 = circulo24 - 1) {
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(x, y, 20 * circulo24, 20 * circulo24);
+    }
+  }
+
+
+
+  //Dibujo para contar minutos//
+
+
+  if (misMinutos <= 1 && misMinutos < 5) {
+
+    for (var circulomin = 1; circulomin > 0; circulomin = circulomin - 1) {
+      noFill();
+      stroke(0, 0, 255);
+      ellipse(x2, y, 3 * circulomin * 5, 3 * circulomin * 5);
+    }
+  }
+
+  if (misMinutos >= 6 && misMinutos <= 10) {
+
+    for (var circulomin2 = 2; circulomin2 > 0; circulomin2 = circulomin2 - 1) {
+      noFill();
+      stroke(0, 0, 255);
+      ellipse(x2, y, 3 * circulomin2 * 5, 3 * circulomin2 * 5);
+    }
+  }
+
+  if (misMinutos >= 11 && misMinutos <= 15) {
+
+    for (var circulomin3 = 3; circulomin3 > 0; circulomin3 = circulomin3 - 1) {
+      noFill();
+      stroke(0, 0, 255);
+      ellipse(x2, y, 3 * circulomin3 * 5, 3 * circulomin3 * 5);
+    }
+  }
+
+
+  if (misMinutos >= 55 && misMinutos <= 60) {
+
+    for (var circulominf = 12; circulominf > 0; circulominf = circulominf - 1) {
+      noFill();
+      stroke(0, 0, 255);
+      ellipse(x2, y, 3 * circulominf * 5, 3 * circulominf * 5);
+    }
+  }
+
+  //conteno de minutos
+
+  //dibujo
+
+  if (misMinutos == misMinutos) {
+
+
+    for (var circulos = 59; circulos > 0; circulos = circulos - 1) {
+      noFill();
+      stroke(0, 0, 255);
+      ellipse(x2, y, 5 * circulos + 5, 5 * circulos + 5);
+    }
+  }
+  //conteo de minutos
+
+  //me indica el movimiento//
+  noFill();
+  stroke(255, 255, 255);
+  ellipse(x2, y, misMinutos * 5, misMinutos * 5);
+
+
+  //dibujo para contar segundos
+
+  noFill();
+  stroke(0, 255, 0);
+  ellipse(x3, y, 60, 60);
+
+  //conteo de segundos//
+  noFill();
+  stroke(255, 255, 255);
+  ellipse(x3, y, misSegundos, misSegundos);
+
+}
