a3874a14a77bc7377bccec9d3868135be2861ae9
memeperez <31419406+memeperez@users.noreply.github.com>
2017-10-23 09:29:32 -0500

    Add files via upload

---
+++ 06A/sketch.js
@@ -0,0 +1,54 @@
+var miImagen;
+
+var x =0;
+var y=0;
+
+function preload(){
+  miImagen = loadImage('assets/caballos.jpg');
+}
+
+function setup() { 
+  createCanvas(850, 599);
+  
+  
+} 
+
+function draw() { 
+   background(220);
+  
+  var pix = miImagen.get(x,y);
+  print(pix);
+
+  var miRojo = pix[0];
+  var miVerde = pix[1];
+  var miAzul = pix[2];
+  var mitrans = pix[3];
+  
+  
+   //if( miRojo >= 0 && miRojo < 100 ){
+
+  pix[0] = 255;
+  pix[2] = 20;
+  pix[3] = 150;
+  
+ 
+   //}
+  
+  miImagen.set(x,y, pix);
+  miImagen.updatePixels();
+  
+
+  image(miImagen, 0,0);
+  filter(OPAQUE);
+  
+
+
+  x = x + 1;
+  
+  if( x > miImagen.width){
+    x = 0;
+    y = y + 5;
+    
+    }
+
+}
